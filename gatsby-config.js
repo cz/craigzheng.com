5c98978fc11904f1502966cd990113eebc401f22
Craig Zheng <craig.zheng@gmail.com>
2019-03-14 13:26:07 -0400

    Fresh start

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,19 @@ module.exports = {
     title: `craigzheng.com`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/content/projects`,
+      },
+    },
   ],
 }
